85afbeafac52e503c23706ba67ddc2e6a83d4e14
NicholasNeto <nicholasneto@outlook.com>
2021-03-19 19:12:21 -0300

    used new modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,79 @@
-import React from 'react';
+
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NNModal } from './components/Modal';
 import { GlobalStyle } from './styles/global'
 
 import { createServer } from 'miragejs'
+import { useState } from 'react';
 
 
 createServer({
-  routes(){
+  routes() {
     this.namespace = 'api'
 
-   this.get('transactions', () => {
-     return [
-       {
-         id: 1,
-         title: 'Salario',
-         amount: 400,
-         type: 'deposit',
-         category: 'Desenvolvimento',
-         createdAt: new Date(),
-       }, 
-       {
-        id: 2,
-        title: 'Transaction 1',
-        amount: 1000,
-        type: 'withdraw',
-        category: 'Food',
-        createdAt: new Date(),
-      }, 
-      {
-        id: 3,
-        title: 'Desenvolvimento de Website',
-        amount: 500,
-        type: 'deposit',
-        category: 'Work',
-        createdAt: new Date(),
-      },
-      {
-        id: 4,
-        title: 'Aluguel',
-        amount: 600,
-        type: 'withdraw',
-        category: 'Aluguel',
-        createdAt: new Date(),
-      },
-     ]
-   })
-    
+    this.get('transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Salario',
+          amount: 400,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Transaction 1',
+          amount: 1000,
+          type: 'withdraw',
+          category: 'Food',
+          createdAt: new Date(),
+        },
+        {
+          id: 3,
+          title: 'Desenvolvimento de Website',
+          amount: 500,
+          type: 'deposit',
+          category: 'Work',
+          createdAt: new Date(),
+        },
+        {
+          id: 4,
+          title: 'Aluguel',
+          amount: 600,
+          type: 'withdraw',
+          category: 'Aluguel',
+          createdAt: new Date(),
+        },
+      ]
+    })
+
   }
 })
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NNModal 
+        isGenericModalOpen={isNewTransactionModalOpen}
+        onCloseGenericModal={handleCloseNewTransactionModal} >
+          <h1> Tetse </h1>
+      </NNModal>
       <GlobalStyle />
     </>
   );
