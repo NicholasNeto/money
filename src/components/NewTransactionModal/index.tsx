184cdd33b87e0abe113a5e2f23c6f0f4734f8c52
NicholasNeto <nicholasneto@outlook.com>
2021-03-19 21:17:32 -0300

    add styles for modal in file global

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,28 @@
 import Modal from 'react-modal'
+import { Container } from './styles'
 
 
 interface NNmodalProps {
     isOpen: boolean;
     onRequestClose: () => void;
-} 
+}
 
-export function NewTransactionModal({isOpen, onRequestClose }: NNmodalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NNmodalProps) {
     return (
         <Modal
             isOpen={isOpen}
-            onRequestClose={onRequestClose}>
+            onRequestClose={onRequestClose}
+            overlayClassName='react-modal-overlay'
+            className='react-modal-content'>
+
+            <Container>
+                <h2>Cadastrar transação</h2>
+                <input type="text" placeholder="Titulo" />
+                <input type="number" placeholder="Valor" />
+                <input type="text" placeholder="Categoria" />
+                <button type="submit">Cadastrar</button>
+            </Container>
+
         </Modal>
     )
 }
