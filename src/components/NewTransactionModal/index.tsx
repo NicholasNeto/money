4ffd3dfa75d6e1d21bf508d3bbafde0b8d456197
NicholasNeto <nicholasneto@outlook.com>
2021-03-19 21:49:34 -0300

    estilizando modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal'
 import { Container } from './styles'
+import closeImg from '../../assets/close.svg'
 
 
 interface NNmodalProps {
@@ -15,6 +16,12 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NNmodalProps) {
             overlayClassName='react-modal-overlay'
             className='react-modal-content'>
 
+            <button
+                type='button'
+                onClick={onRequestClose}
+                className='react-modal-close'>
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
             <Container>
                 <h2>Cadastrar transação</h2>
                 <input type="text" placeholder="Titulo" />
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,57 @@
 import styled from 'styled-components'
 
 export const Container = styled.form`
+    h2 {
+        color: var(--text-title);
+        font-size: 1.5rem;
+        margin-bottom: 2rem;
+    }
 
+    input {
+
+        width: 100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+
+
+   
+        background: #E7E9EE;
+
+        border: 1px solid #D7D7D7;
+        box-sizing: border-box;
+        border-radius: 0.25px;
+
+
+        font-size: 1rem;
+        font-weight: 600;
+
+        &::placeholder {
+          color: var(--text-body)  
+        }
+
+
+        & + input {
+            margin-top: 1rem;
+        }
+
+    }
+
+    button[type="submit"] {
+        width: 100%;
+        padding: 0 1.5rem;
+        height: 4rem;
+        background: var(--green);
+        color: #FFF;
+        border-radius: 0.25rem;
+        border: 0;
+        font-size: 1rem;
+        font-weight: 600;
+        margin-top: 1.5rem;
+
+        transition: filter 0.2s;
+
+        &:hover {
+            filter: brightness(0.9);
+        }
+    }
 `;
